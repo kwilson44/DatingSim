640f612493c6a0eceb2691a2b7a79297b58e3b13
stemgirl3000 <67025172+stemgirl3000@users.noreply.github.com>
2022-12-12 13:49:57 -0500

    :D

--- Player.java
+++ Player.java
@@ -6,9 +6,13 @@ import java.util.ArrayList;
 public class Player {
   private ArrayList<String> inventory = new ArrayList<String>(); 
   Integer points;
+  String name;
+  String userHouse;
   
-  public Player(String name, int classYear, String House) {
+  public Player(String name, int classYear, String house) {
     this.inventory = new ArrayList<String>(); 
     this.points = 0;
+    this.name = name;
+    this.userHouse = house;
   }
 }
